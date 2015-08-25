70ab85c05dfdc28062cabde214d48ec0975cab00
stearagon <stearagon@gmail.com>
2015-08-25 17:17:51 -0400

    Add new and create to Topics controller.

--- app/controllers/api/topics_controller.rb
+++ app/controllers/api/topics_controller.rb
@@ -1,2 +1,20 @@
 class Api::TopicsController < ApplicationController
+  def new
+    @topic = Topic.new
+  end
+
+  def create
+    @topic = Topic.new(topic_params)
+
+    if @topic
+      render json: @topic
+    else
+      render :new
+    end
+  end
+
+  private
+  def topic_params
+    params.require(:topic).permit(:title)
+  end
 end
