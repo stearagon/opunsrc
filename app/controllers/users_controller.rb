eaa356dfc2647f784bf09c958611a1db52249914
stearagon <stearagon@gmail.com>
2015-09-01 02:44:01 -0400

    Add sign_in and sign_out method to application controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user
+      sign_in(user)
       redirect_to user_url(@user)
     else
       flash.now[:errors] = @user.errors.full_messages
