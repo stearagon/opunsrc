d7594c606b786229b5a459eb844a6cc2dacf1ff8
stearagon <stearagon@gmail.com>
2015-08-31 23:04:31 -0400

    Add delete and add links to show pages.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  root 'api/topics#index'
 
-  namespace :api do # , defaults: { format: :json } 
+  namespace :api do # , defaults: { format: :json }
     resources :topics
     resources :puns
   end
